e54321a3aebfe31ec6b7bb397c0bc06a10e1151c
oseba <omrseba@gmail.com>
2019-02-04 17:45:26 +0100

    modification du script PaletsZoneChaos  Rectification des actions(ajout d'une rotation)

--- T3HL-master/src/main/java/scripts/PaletsZoneChaos.java
+++ T3HL-master/src/main/java/scripts/PaletsZoneChaos.java
@@ -29,15 +29,20 @@ public class PaletsZoneChaos extends Script{
 
     @Override
     public void execute(Integer version) {
+        boolean premierPaletPris = false;
         try{
             robot.turn(Math.PI/2);
             robot.useActuator(ActuatorsOrder.ACTIVE_LA_POMPE_DROITE);
             for (Vec2 position : positions) {
+                if(premierPaletPris == true){
+                    robot.turn(-Math.PI);
+                }
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_SOL);
                 robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DROITE);
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_ASCENSEUR);
                 robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_DROITE);
                 robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_DROIT_DE_UN_PALET);
+                premierPaletPris = true;
             }
         robot.useActuator(ActuatorsOrder.DESACTIVE_LA_POMPE_GAUCHE);
         }catch (UnableToMoveException e) {
