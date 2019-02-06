67b020a9a6e7b3e99ddf715791ad99b10f77e6a5
Mikail <mikail.duzenli@telecom-sudparis.eu>
2019-02-06 16:13:01 +0100

    ajout de tous les palets dans la classe palet

--- T3HL-common/src/main/java/data/Palet.java
+++ T3HL-common/src/main/java/data/Palet.java
@@ -3,12 +3,36 @@ package data;
 import data.table.StillCircularObstacle;
 
 public enum Palet {
-    ROUGE_1_ZONE_DEPART(CouleurPalet.ROUGE, false),
-    ROUGE_2_ZONE_DEPART(CouleurPalet.ROUGE, false),
-    VERT_ZONE_DEPART(CouleurPalet.VERT, false),
-    ROUGE_1_ZONE_CHAOS(CouleurPalet.ROUGE,false),
-    ROUGE_2_ZONE_CHAOS(CouleurPalet.ROUGE,false),
-    
+    ROUGE_1_ZONE_DEPART_US(CouleurPalet.ROUGE, false),
+    ROUGE_2_ZONE_DEPART_US(CouleurPalet.ROUGE, false),
+    VERT_ZONE_DEPART_US(CouleurPalet.VERT, false),
+    ROUGE_1_ZONE_CHAOS_US(CouleurPalet.ROUGE,false),
+    ROUGE_2_ZONE_CHAOS_US(CouleurPalet.ROUGE,false),
+    BLEU_ZONE_CHAOS_US(CouleurPalet.BLEU,false),
+    VERT_ZONE_CHAOS_US(CouleurPalet.VERT,false),
+    ROUGE_1_ZONE_CHAOS_ENNEMI(CouleurPalet.ROUGE,false),
+    ROUGE_2_ZONE_CHAOS_ENNEMI(CouleurPalet.ROUGE,false),
+    BLEU_ZONE_CHAOS_ENNEMI(CouleurPalet.BLEU,false),
+    VERT_ZONE_CHAOS_ENNEMI(CouleurPalet.VERT,false),
+    ROUGE_1_ZONE_X6_US(CouleurPalet.ROUGE,false),
+    VERT_1_ZONE_X6_US(CouleurPalet.VERT,false),
+    ROUGE_2_ZONE_X6_US(CouleurPalet.ROUGE,false),
+    VERT_2_ZONE_X6_US(CouleurPalet.VERT,false),
+    ROUGE_3_ZONE_X6_US(CouleurPalet.ROUGE,false),
+    BLEU_ZONE_X6_US(CouleurPalet.BLEU,false),
+    ROUGE_1_ZONE_X6_ENNEMI(CouleurPalet.ROUGE,false),
+    VERT_1_ZONE_X6_ENNEMI(CouleurPalet.VERT,false),
+    ROUGE_2_ZONE_X6_ENNEMI(CouleurPalet.ROUGE,false),
+    VERT_2_ZONE_X6_ENNEMI(CouleurPalet.VERT,false),
+    ROUGE_3_ZONE_X6_ENNEMI(CouleurPalet.ROUGE,false),
+    BLEU_ZONE_X6_ENNEMI(CouleurPalet.BLEU,false),
+    ROUGE_ZONE_X3(CouleurPalet.ROUGE,false),
+    BLEU_ZONE_X3(CouleurPalet.BLEU,false),
+    VERT_ZONE_X3(CouleurPalet.VERT,false),
+
+
+
+
 
     ;
     private CouleurPalet couleur;
