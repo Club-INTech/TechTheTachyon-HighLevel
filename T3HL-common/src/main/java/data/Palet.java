0b4b265ec719177a8f09bd684a912dcf65dd4e71
elomhs <eloise.maheas@telecom-sudparis.eu>
2019-02-06 14:58:16 +0100

    debut ajout des différents palets

--- T3HL-common/src/main/java/data/Palet.java
+++ T3HL-common/src/main/java/data/Palet.java
@@ -3,9 +3,19 @@ package data;
 import data.table.StillCircularObstacle;
 
 public enum Palet {
+    ROUGE_1_ZONE_DEPART(CouleurPalet.ROUGE, false),
+    ROUGE_2_ZONE_DEPART(CouleurPalet.ROUGE, false),
+    VERT_ZONE_DEPART(CouleurPalet.VERT, false),
+    ROUGE_1_ZONE_CHAOS(CouleurPalet.ROUGE,false),
+    ROUGE_2_ZONE_CHAOS(CouleurPalet.ROUGE,false),
+    
 
     ;
     private CouleurPalet couleur;
-    private StillCircularObstacle obstacle;
     private boolean palet_pris;
+
+    Palet(CouleurPalet couleur, boolean palet_pris) {
+        this.couleur = couleur;
+        this.palet_pris = palet_pris;
+    }
 }
