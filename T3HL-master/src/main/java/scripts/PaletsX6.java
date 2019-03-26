64405ae8bbd341c7d525bb9ad0354f6d95e314b8
Victor Clouet d'Orval <victorclouetdorval@orange.fr>
2019-03-26 14:57:56 +0100

    pallets x6 a pris ses positions en version 0

--- T3HL-master/src/main/java/scripts/PaletsX6.java
+++ T3HL-master/src/main/java/scripts/PaletsX6.java
@@ -36,9 +36,9 @@ public class PaletsX6 extends Script {
      Autrement dit on divise la demi table en deux et selon cela on choisit de commencer à droite ou à gauche du distributeur
      */
         if (version == 0) {
-            positions.add(new VectCartesian(0, 800));
-            positions.add(new VectCartesian(0, 800));
-            positions.add(new VectCartesian(0, 800));
+            positions.add(new VectCartesian(114, 50));
+            positions.add(new VectCartesian(114, 60));
+            positions.add(new VectCartesian(114, 70));
         } else if (version == 1) {
             positions.add(new VectCartesian(0, 800));
             positions.add(new VectCartesian(0, 800));
@@ -84,7 +84,7 @@ public class PaletsX6 extends Script {
     public Shape entryPosition(Integer version) {
 
         if (version == 0) {
-            Shape positionEntree = new Circle(new VectCartesian(0,800), 5);
+            Shape positionEntree = new Circle(new VectCartesian(114,50), 5);
             return positionEntree;
         }
         else if (version == 1) {
