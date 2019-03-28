8c8605201f0cd5c3118ff1d2c52604fcf0f2b604
gwen <gwendoline.serandour@telecom-sudparis.eu>
2019-03-28 18:47:18 +0100

    rajout de message dans communicationWithBuddy

--- T3HL-master/src/main/java/CommunicationWithBuddy.java
+++ T3HL-master/src/main/java/CommunicationWithBuddy.java
@@ -1,17 +1,52 @@
 import connection.Connection;
 import data.Palet;
+import data.XYO;
+import orders.order.Order;
 import pfg.config.Config;
+import scripts.Script;
 import utils.ConfigData;
+import utils.Container;
 import utils.communication.CommunicationException;
+import utils.container.ContainerException;
 import utils.container.Service;
 
-public class CommunicationWithBuddy implements Service {
+import java.util.Optional;
+
+public class CommunicationWithBuddy extends Thread implements Service {
 
     private boolean symetry;
 
     private boolean simulation;
 
     private Connection ConnectionWithBuddy;
+
+    private Container container;
+
+    public CommunicationWithBuddy(Container container){
+        this.container=container;
+    }
+
+    @Override
+    public void run() {
+        while (!isInterrupted()) {
+            try {
+                Optional<String> message = ConnectionWithBuddy.read();
+                if(message.isPresent()) {
+                    String msg =message.get();
+                }
+
+
+            } catch (CommunicationException e) {
+                e.printStackTrace();
+            }
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     public void sendString(String message) {
         try {
@@ -25,6 +60,26 @@ public class CommunicationWithBuddy implements Service {
             } catch (CommunicationException ef) {
                 ef.printStackTrace();
             }
+        }
+    }
+
+    public void sendPosition(){
+        this.sendString("Pos "+ XYO.getRobotInstance());
+    }
+
+    public void sendOrder(Script script){
+        this.sendString("Order "+ script.getClass().getCanonicalName());
+    }
+
+    public void doScript(String scriptName,int version){
+        try {
+            Class scriptClass = Class.forName(scriptName);
+            Script script = (Script)container.getService(scriptClass);
+            script.goToThenExecute(version);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (ContainerException e) {
+            e.printStackTrace();
         }
     }
 
