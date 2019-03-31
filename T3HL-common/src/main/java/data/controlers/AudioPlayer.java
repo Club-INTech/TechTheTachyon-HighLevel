0f3f31f334f60a6c736e91605e0af0da1b07bd48
INTechLucas <lucas.molia@telecom-sudparis.eu>
2019-04-01 00:19:16 +0200

    Ajout de ma magnifique classe AudioPlayer pour balancer du gros son sur le robot

---
+++ T3HL-common/src/main/java/data/controlers/AudioPlayer.java
@@ -0,0 +1,86 @@
+package data.controlers;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class AudioPlayer {
+
+    private AudioInputStream audioInputStream;
+    private AudioFormat audioFormat;
+    private SourceDataLine audioLine;
+
+    private Properties audioNames;
+
+    public AudioPlayer(){
+        audioNames = new Properties();
+
+        FileInputStream input = null;
+        try {
+            input = new FileInputStream("../config/audioFiles.config");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            audioNames.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadFile(String name) {
+        File file = new File(audioNames.getProperty(name));
+
+        try{
+            audioInputStream = AudioSystem.getAudioInputStream(file);
+
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+            return;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        audioFormat = audioInputStream.getFormat();
+
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+
+        try {
+            audioLine = (SourceDataLine) AudioSystem.getLine(info);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play() {
+        try {
+            audioLine.open(audioFormat);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        audioLine.start();
+
+        try {
+            byte bytes[] = new byte[1024];
+            int bytesRead=0;
+            while (((bytesRead = audioInputStream.read(bytes, 0, bytes.length)) != -1)) {
+
+                audioLine.write(bytes, 0, bytesRead);
+            }
+        } catch (IOException io) {
+            io.printStackTrace();
+            return;
+        }
+
+        audioLine.close();
+    }
+
+
+}
