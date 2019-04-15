5b2d97b6ba696708d84fc65c609a680371a86c5d
emmanuel.allaire <emmanuel.allaire@telecom-sudparis.eu>
2019-04-15 23:44:24 +0200

    avancement scripts secondaire

--- T3HL-slave/src/main/java/scripts/Cracheur.java
+++ T3HL-slave/src/main/java/scripts/Cracheur.java
@@ -17,7 +17,8 @@ public class Cracheur extends Script {
     private int xEntry = 2000;
     private int yEntry = 2000;
 
-    private int nbPalets = 3;
+    private int nbPalets = robot.getNbPaletsDroits();
+    private int nbMaxPalets = 5; //Nb de palets si l'ascenseur est rempli pour distinction de cas
 
 
     public Cracheur(Slave robot, Table table) {
@@ -26,10 +27,21 @@ public class Cracheur extends Script {
 
     @Override
     public void execute(Integer version) {
-        //situation de départ avec 3 palets dans l'ascenseur
-        for (int j = 1; j<= nbPalets; j++) {
-            robot.useActuator(ActuatorsOrder.MONTE_ASCENSEUR_DU_SECONDAIRE_DE_UN_PALET);
+        if (nbPalets < nbMaxPalets) {
+            for (int j = 1; j<= nbPalets; j++) {
+                robot.useActuator(ActuatorsOrder.MONTE_ASCENCEUR_DROIT_DE_UN_PALET);
+                robot.useActuator(ActuatorsOrder.CRACHE_UN_PALET);
+                robot.popPaletDroit();
+            }
+        }
+        else {
+            for (int i = 1; i < nbPalets; i++) {
+                robot.useActuator(ActuatorsOrder.CRACHE_UN_PALET);
+                robot.popPaletDroit();
+                robot.useActuator(ActuatorsOrder.MONTE_ASCENCEUR_DROIT_DE_UN_PALET);
+            }
             robot.useActuator(ActuatorsOrder.CRACHE_UN_PALET);
+            robot.popPaletDroit();
         }
     }
     @Override //à adapter
--- T3HL-slave/src/main/java/scripts/Paletsx3.java
+++ T3HL-slave/src/main/java/scripts/Paletsx3.java
@@ -37,31 +37,31 @@ public class Paletsx3 extends Script {
     public void execute(Integer version) {
         try {
             robot.turn(Math.PI);
-            robot.useActuator(ActuatorsOrder.ACTIVE_LA_POMPE_DU_SECONDAIRE);
+            robot.useActuator(ActuatorsOrder.ACTIVE_LA_POMPE_DROITE);
             for (int j = 1; j < positions.length; j++) {
-                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_DISTRIBUTEUR);
-                robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_DU_SECONDAIRE);
-                robot.useActuator(ActuatorsOrder.REMONTE_LE_BRAS_DU_SECONDAIRE_DU_DISTRIBUTEUR_VERS_ASCENSEUR);
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_DISTRIBUTEUR);
+                robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_DROITE);
+                robot.useActuator(ActuatorsOrder.REMONTE_LE_BRAS_DROIT_DU_DISTRIBUTEUR_VERS_ASCENSEUR);
 
-                robot.useActuator(ActuatorsOrder.TEST_PALET_ATTRAPÉ_EN_FONCTION_DU_COUPLE_DU_SECONDAIRE);
+                robot.useActuator(ActuatorsOrder.TEST_PALET_ATTRAPÉ_EN_FONCTION_DU_COUPLE_DROIT);
                 CouleurPalet couleur = CouleurPalet.getCouleurPalRecu();
 
-                robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DU_SECONDAIRE);
-                robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_DU_SECONDAIRE_DE_UN_PALET);
+                robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DROITE);
+                robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_DROIT_DE_UN_PALET);
 
-                ((Slave) robot).pushPaletDroit(couleur); // TODO
+                robot.pushPaletDroit(couleur); // TODO
                 robot.followPathTo(positions[j]);
             }
-            robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_DISTRIBUTEUR);
-            robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_DU_SECONDAIRE);
-            robot.useActuator(ActuatorsOrder.REMONTE_LE_BRAS_DU_SECONDAIRE_DU_DISTRIBUTEUR_VERS_ASCENSEUR);
-            robot.useActuator(ActuatorsOrder.TEST_PALET_ATTRAPÉ_EN_FONCTION_DU_COUPLE_DU_SECONDAIRE);
+            robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_DISTRIBUTEUR);
+            robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_DROITE);
+            robot.useActuator(ActuatorsOrder.REMONTE_LE_BRAS_DROIT_DU_DISTRIBUTEUR_VERS_ASCENSEUR);
+            robot.useActuator(ActuatorsOrder.TEST_PALET_ATTRAPÉ_EN_FONCTION_DU_COUPLE_DROIT);
 
             CouleurPalet couleur = CouleurPalet.getCouleurPalRecu();
-            robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DU_SECONDAIRE);
-            robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_DU_SECONDAIRE_DE_UN_PALET);
-            ((Slave) robot).pushPaletDroit(couleur); // TODO
-            robot.useActuator(ActuatorsOrder.DESACTIVE_LA_POMPE_DU_SECONDAIRE);
+            robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DROITE);
+            robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_DROIT_DE_UN_PALET);
+            robot.pushPaletDroit(couleur); // TODO
+            robot.useActuator(ActuatorsOrder.DESACTIVE_LA_POMPE_DROITE);
 
         }
         catch (UnableToMoveException e) {
