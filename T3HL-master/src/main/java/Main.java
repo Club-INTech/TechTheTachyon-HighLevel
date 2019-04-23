d16ef7be5028365f29a07eebcd56182f8a4fcc9b
rene-INTech <remi.lamiaux@telecom-sudparis.eu>
2019-04-23 15:29:46 +0200

    je recommente parce que ca marche pas

--- T3HL-master/src/main/java/Main.java
+++ T3HL-master/src/main/java/Main.java
@@ -87,13 +87,13 @@ public class Main {
         if (container.getConfig().getBoolean(ConfigData.SIMULATION)) {
             initSimulator();
         }
-
+/*
         try {
             panneauService.getPaneau().printScore(505);
         }catch (IOException | TooManyDigitsException e){
             e.printStackTrace();
         }
-
+*/
         try {
             initAI();
         } catch (ContainerException e) {
