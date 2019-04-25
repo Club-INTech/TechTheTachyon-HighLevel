eedbe412f51dd3cbb26adca1af3602da6d988947
Nayht <william.huver@telecom-sudparis.eu>
2019-04-25 19:09:38 +0200

    Ajout du support des getters et setters pour les palets de la zone chaos

--- T3HL-common/src/main/java/data/PaletsZoneChaos.java
+++ T3HL-common/src/main/java/data/PaletsZoneChaos.java
@@ -27,19 +27,68 @@ public enum PaletsZoneChaos {
     private boolean palet_marque;
     private Vec2 position;
 
+    /**
+     * Constructeur
+     * @param id id du palet
+     * @param couleur couleur du palet
+     * @param palet_pris est-ce que le palet a été pris ?
+     * @param palet_marque est-ce que le palet a été déposé pour marquer des points ?
+     * @param position position du palet sur la table
+     */
     PaletsZoneChaos(int id, CouleurPalet couleur, boolean palet_pris , boolean palet_marque, Vec2 position) {
+        this.id = id;
         this.couleur = couleur;
         this.palet_pris = palet_pris;
         this.palet_marque = palet_marque;
-        this.id = id;
         this.position = position;
     }
 
+    /**
+     * Set la position du palet
+     * @param position Vec2 de la position
+     */
     public void setPosition(Vec2 position) {
         this.position = position;
     }
 
+    /**
+     * Retourne la position du palet
+     * @return Vec2 de la position du palet
+     */
     public Vec2 getPosition() {
         return position;
     }
+
+    /**
+     * Set si la palet a été pris
+     * @param value True si le palet a été pris, False sinon
+     */
+    public void setPaletPris(boolean value){
+        this.palet_pris = value;
+    }
+
+    /**
+     * Renvoie si le palet a été pris
+     * @return True si le palet a été pris, False sinon
+     */
+    public boolean getPaletPris(){
+        return this.palet_pris;
+    }
+
+    /**
+     * Set si le palet a été déposé pour marquer des points
+     * @param value True si c'est le cas, False sinon
+     */
+    public void setPaletMarque(boolean value){
+        this.palet_marque = value;
+    }
+
+    /**
+     * Get si le palet a été déposé pour marquer des points
+     * @return True si c'est le cas, False sinon
+     */
+    public boolean getPaletMarque(){
+        return this.palet_marque;
+    }
+
 }
