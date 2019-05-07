9b81ffd310d17232cd82d44f4e3315fbc8cb73b3
jglrxavpok <jglrxavpok@gmail.com>
2019-05-08 00:27:15 +0200

    Envoi des *deux* bras vers l'ascenseur au démarrage du HL

--- T3HL-master/src/main/java/MainMaster.java
+++ T3HL-master/src/main/java/MainMaster.java
@@ -128,7 +128,7 @@ public class MainMaster extends RobotEntryPoint {
     protected void act() throws UnableToMoveException {
         XYO.getRobotInstance().update(1500-191, 1750, Math.PI ); // /!\Je l'ai mis en haut à gauche
         robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_ASCENSEUR);
-        // FIXME    robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_ASCENSEUR);
+        robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_ASCENSEUR);
         robot.setPositionAndOrientation(XYO.getRobotInstance().getPosition(), XYO.getRobotInstance().getOrientation());
 
         if(container.getConfig().getString(ConfigData.COULEUR).equals("violet")) {
