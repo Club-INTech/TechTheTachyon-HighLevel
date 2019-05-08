30982b6601fd399fecc250181fbc7d282d24d634
oseba <omrseba@gmail.com>
2019-05-08 19:37:22 +0200

    test script

--- T3HL-master/src/main/java/scripts/PaletsZoneDepart.java
+++ T3HL-master/src/main/java/scripts/PaletsZoneDepart.java
@@ -47,6 +47,7 @@ public class PaletsZoneDepart extends Script {
                 }
                 //robot.turn(Math.PI / 2);
                 robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_GAUCHE, true);
+                for(int f =0;f<100;f++) { }
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_SOL,true);
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_ASCENSEUR,true);
                 robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_GAUCHE, true); // on attend que le vide se casse
