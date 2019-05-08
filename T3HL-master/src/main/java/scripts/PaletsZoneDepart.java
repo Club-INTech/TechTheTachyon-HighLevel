4125ca2d72546692f78f1461cbb903163160ad9b
oseba <omrseba@gmail.com>
2019-05-08 19:59:37 +0200

    test script

--- T3HL-master/src/main/java/scripts/PaletsZoneDepart.java
+++ T3HL-master/src/main/java/scripts/PaletsZoneDepart.java
@@ -18,7 +18,7 @@ public class PaletsZoneDepart extends Script {
 
     private static final int DISTANCE_INTERPALET = 300;
     private int xEntry = 1500-191-65+20;//1350;
-    private int yEntry = 455;
+    private int yEntry = 450;
     private Vec2[] positions = new VectCartesian[]{
             new VectCartesian(xEntry, yEntry),
             new VectCartesian(xEntry,yEntry+300),
@@ -50,6 +50,7 @@ public class PaletsZoneDepart extends Script {
 
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_SOL,true);
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_ASCENSEUR,true);
+                robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_GAUCHE, true); // on attend que le vide se casse
                 robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_GAUCHE, true); // on attend que le vide se casse
                 robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_GAUCHE_DE_UN_PALET);
                 // FIXME: corriger couleur
