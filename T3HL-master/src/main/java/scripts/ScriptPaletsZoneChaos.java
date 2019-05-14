6c9ab445b248a2cdf8b127da135085ba4f014ad5
Mikail <mikail.duzenli@telecom-sudparis.eu>
2019-05-14 20:42:14 +0200

    On fait la même séquence de mouvement que pour zone de départ quand on veut prendre les palets de zone de chaos

--- T3HL-master/src/main/java/scripts/ScriptPaletsZoneChaos.java
+++ T3HL-master/src/main/java/scripts/ScriptPaletsZoneChaos.java
@@ -62,11 +62,17 @@ public class ScriptPaletsZoneChaos extends Script{
             for (Vec2 position : positions) {
                 robot.followPathTo(position);
                 robot.turn(Math.PI/2);
-                    robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_GAUCHE_DE_UN_PALET);
-                    robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_SOL);
-                    robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_GAUCHE,true);
-                    robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_DEPOT);
-                    robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_GAUCHE,true);
+                robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_GAUCHE_DE_UN_PALET);
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_SOL);
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_DEPOT);
+                robot.useActuator(ActuatorsOrder.DESACTIVE_LA_POMPE_GAUCHE,true);
+                robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_GAUCHE,true);
+                robot.useActuator(ActuatorsOrder.DESCEND_MONTE_ASCENCEUR_GAUCHE_DE_UN_PALET,true);
                     /*
                     //Ce qui suit c'est du piff j'ai la flemme de vérifier la couleur parcequ'on s'en fou
                     if (position == positions[0]){
