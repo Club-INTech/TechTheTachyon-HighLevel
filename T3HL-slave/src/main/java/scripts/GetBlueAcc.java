28bfa57b524ec09373002de04f84929449f33fcf
jglrxavpok <jglrxavpok@gmail.com>
2019-05-15 20:57:32 +0200

    Recalage en Y au niveau du palet bleu

--- T3HL-slave/src/main/java/scripts/GetBlueAcc.java
+++ T3HL-slave/src/main/java/scripts/GetBlueAcc.java
@@ -1,6 +1,8 @@
 package scripts;
 
+import data.Sick;
 import data.Table;
+import data.XYO;
 import locomotion.UnableToMoveException;
 import orders.order.ActuatorsOrder;
 import pfg.config.Config;
@@ -15,6 +17,21 @@ public class GetBlueAcc extends Script {
     private int xBlue = -210; //FIXME: positions à faire
     private int yBlue = 270+60;
     private boolean symetrie;
+
+    /**
+     * Offset avec la planche
+     */
+    private final int offsetRecalage = 31;
+
+    /*
+     * Offset pour corriger la mesure des sicks (différence réel - mesuré)
+     */
+    private final int offsetSick= 6;
+
+    /**
+     * Différence en Y et X entre le sick et le centre du robot
+     */
+    private final int ySickToRobotCenter=100;
 
     public GetBlueAcc(Slave robot, Table table) {
         super(robot, table);
@@ -40,15 +57,27 @@ public class GetBlueAcc extends Script {
         robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_DU_SECONDAIRE_DE_UN_PALET);*/
 
         // Nouvelle strat: on va pousser le bleu en premier, en faisant un arc de cercle avec le bras du secondaire
-
         try {
             robot.turn(Math.PI);
             robot.followPathTo(new VectCartesian(xBlue, yBlue));
+
+            // === Début recalage ===
+            double dsick = 64;
+            int ecart_mesures_sicks=Sick.SICK_AVANT_GAUCHE.getLastMeasure() - Sick.SICK_ARRIERE_GAUCHE.getLastMeasure();
+            double rapport = ecart_mesures_sicks / dsick;
+            double teta = Math.atan(rapport);
+
+            float averageDistance = (float) (Math.cos(teta)*((Sick.SICK_AVANT_GAUCHE.getLastMeasure() + Sick.SICK_ARRIERE_GAUCHE.getLastMeasure()) / 2 + this.offsetSick + this.ySickToRobotCenter) + offsetRecalage);
+
+            Vec2 currentPosition = XYO.getRobotInstance().getPosition();
+            robot.gotoPoint(new VectCartesian(currentPosition.getX(), currentPosition.getY() + this.yBlue - averageDistance));
+            // === Fin recalage ===
+
             if (!symetrie) {
                 robot.turn(Math.PI/2);
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ACCELERATEUR_SECONDAIRE);
                 robot.turn(0);
-                robot.turn(-0.78);
+                robot.turn(-0.78/2);
                 // voir si c'est nécessaire robot.turn(0);
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ASCENSEUR);
             }
