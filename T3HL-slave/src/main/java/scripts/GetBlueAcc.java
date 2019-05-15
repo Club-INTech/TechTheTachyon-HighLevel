45fbe70e78587c0841bab1db261dd89a1759d797
bapt117 <baptiste.ghirardi@telecom-sudparis.eu>
2019-05-15 12:45:21 +0200

    modifs getBlueAcc pour pousser le palet dans l'acc (penser à modifier les positions)

--- T3HL-slave/src/main/java/scripts/GetBlueAcc.java
+++ T3HL-slave/src/main/java/scripts/GetBlueAcc.java
@@ -14,6 +14,8 @@ import utils.math.VectCartesian;
 public class GetBlueAcc extends Script {
     private int xEntry = -170;
     private int yEntry = 340;
+    private int xBlue = 0; //FIXME: positions à faire
+    private int yBlue = 0;
     private boolean symetrie;
 
     public GetBlueAcc(Slave robot, Table table) {
@@ -22,7 +24,7 @@ public class GetBlueAcc extends Script {
 
     @Override
     public void execute(Integer version) {
-        try {
+        /*try {
             if (!symetrie){
                 robot.turn(0);
             }
@@ -37,7 +39,43 @@ public class GetBlueAcc extends Script {
         robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_DU_SECONDAIRE);
         robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ASCENSEUR);
         robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DU_SECONDAIRE);
-        robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_DU_SECONDAIRE_DE_UN_PALET);
+        robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_DU_SECONDAIRE_DE_UN_PALET);*/
+
+        // Nouvelle strat: on va pousser le bleu en premier, en faisant un arc de cercle avec le bras du secondaire
+
+        try {
+            if (!symetrie) {
+                robot.turn(Math.PI);
+                robot.moveLengthwise(1000, false);
+                robot.followPathTo(new VectCartesian(xBlue, yBlue));
+                robot.turn(Math.PI/2);
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ACCELERATEUR);
+                robot.turn(0);
+                robot.turn(-0.78);
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ASCENSEUR);
+                robot.turn(Math.PI);
+
+
+            }
+            else {
+                robot.turn(0);
+                robot.moveLengthwise(1000, false);
+                robot.followPathTo(new VectCartesian(-(xBlue), yBlue));
+                robot.turn(-(Math.PI/2));
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ACCELERATEUR);
+                robot.turn(0.78);
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ASCENSEUR);
+                robot.turn(0);
+
+
+            }
+        } catch (UnableToMoveException e) {
+            e.printStackTrace();
+        }
+
+
+
+
     }
     @Override //à adapter
     public Vec2 entryPosition(Integer version) { return new VectCartesian(xEntry, yEntry); }
