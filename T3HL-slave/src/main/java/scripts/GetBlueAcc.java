04a2c950364b468f33d10e860902401f01b2d4f4
Nayht <william.huver@telecom-sudparis.eu>
2019-05-18 12:15:17 +0200

    on rapproche le robot de 1cm de l'accélérateur

--- T3HL-slave/src/main/java/scripts/GetBlueAcc.java
+++ T3HL-slave/src/main/java/scripts/GetBlueAcc.java
@@ -15,7 +15,7 @@ public class GetBlueAcc extends Script {
     //private int xEntry = 1200; WTF les positions
     //private int yEntry = 250;
     private int xBlue = -90; //FIXME: positions à faire
-    private int yBlue = 270+55;
+    private int yBlue = 270+45;
     private boolean symetrie;
 
     /**
