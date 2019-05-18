cc5be0d4a9ef084aa84318fe2142a3d72921823f
oseba <omrseba@gmail.com>
2019-05-18 16:51:58 +0200

    Goldenium: Changement position y

--- T3HL-slave/src/main/java/scripts/Goldenium.java
+++ T3HL-slave/src/main/java/scripts/Goldenium.java
@@ -17,7 +17,7 @@ public class Goldenium extends Script {
     //position d'entrée
 
     private int xEntry = -715 +40; //a tester
-    private int yEntry = 250+ 6  ; //a tester
+    private int yEntry = 250+ 8  ; //a tester
 
     //position de fin
 
