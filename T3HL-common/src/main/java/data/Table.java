55adcaee0ad335526c320cc69cab48b13915bb5c
Nayht <william.huver@telecom-sudparis.eu>
2019-05-18 13:21:03 +0200

    on augmente la largeur en y des obstacles goldenium, car sinon on se les prend avec le secondaire

--- T3HL-common/src/main/java/data/Table.java
+++ T3HL-common/src/main/java/data/Table.java
@@ -205,12 +205,12 @@ public class Table implements Service {
         this.addFixedObstacleNoGraphChange(accelerateur);
 
         Vec2 vecteurGoldeniumDroiteCentre = new VectCartesian(736, 46);
-        CircularRectangle formeGoldeniumDroite = new CircularRectangle(vecteurGoldeniumDroiteCentre, 102, 22, robotRay+obstacleMargin);
+        CircularRectangle formeGoldeniumDroite = new CircularRectangle(vecteurGoldeniumDroiteCentre, 102, 40, robotRay+obstacleMargin);
         Obstacle goldeniumDroite = new StillCircularRectangularObstacle(formeGoldeniumDroite);
         this.addFixedObstacleNoGraphChange(goldeniumDroite);
 
         Vec2 vecteurGoldeniumGaucheCentre = new VectCartesian(-736, 46);
-        CircularRectangle formeGoldeniumGauche = new CircularRectangle(vecteurGoldeniumGaucheCentre, 102, 22, robotRay+obstacleMargin);
+        CircularRectangle formeGoldeniumGauche = new CircularRectangle(vecteurGoldeniumGaucheCentre, 102, 40, robotRay+obstacleMargin);
         Obstacle goldeniumGauche = new StillCircularRectangularObstacle(formeGoldeniumGauche);
         this.addFixedObstacle(goldeniumGauche);
 
