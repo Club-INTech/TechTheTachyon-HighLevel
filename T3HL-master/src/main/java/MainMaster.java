2983519575a4621bc6cfc23338d06ce4e35e20ed
elomhs <eloise.maheas@telecom-sudparis.eu>
2019-05-19 22:55:10 +0200

    on recale le robot puis il bouge jusqu'à sa postion de départ

--- T3HL-master/src/main/java/MainMaster.java
+++ T3HL-master/src/main/java/MainMaster.java
@@ -194,6 +194,7 @@ public class MainMaster extends RobotEntryPoint {
             robot.computeNewPositionAndOrientation(Sick.LOWER_RIGHT_CORNER_TOWARDS_PI);
         }
         robot.turn(Math.PI/2);
+        robot.gotoPoint(new VectCartesian(1500-250,712));
 
         robot.setRotationSpeed(Speed.DEFAULT_SPEED);
         // la symétrie de la table permet de corriger le droit en gauche (bug ou feature?)
