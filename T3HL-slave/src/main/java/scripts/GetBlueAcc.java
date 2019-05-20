48eb8b02d39d4151a4916ce5faa772b76d262bda
elomhs <eloise.maheas@telecom-sudparis.eu>
2019-05-21 01:12:51 +0200

    test getblueacc

--- T3HL-slave/src/main/java/scripts/GetBlueAcc.java
+++ T3HL-slave/src/main/java/scripts/GetBlueAcc.java
@@ -94,6 +94,8 @@ public class GetBlueAcc extends Script {
                 robot.moveLengthwise(-120, false);
                 robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DU_SECONDAIRE,true);
                 robot.moveLengthwise(100,false);
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ASCENSEUR,true);
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ACCELERATEUR);
                 robot.moveLengthwise(-120,false);
                 robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DU_SECONDAIRE,true); // lâche le palet rouge
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ASCENSEUR, true);
@@ -103,6 +105,8 @@ public class GetBlueAcc extends Script {
                 robot.moveLengthwise(120, false);
                 robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DU_SECONDAIRE,true);
                 robot.moveLengthwise(-100,false);
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ASCENSEUR,true);
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ACCELERATEUR);
                 robot.moveLengthwise(120,false);
                 robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DU_SECONDAIRE,true); // lâche le palet rouge
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ASCENSEUR, true);
