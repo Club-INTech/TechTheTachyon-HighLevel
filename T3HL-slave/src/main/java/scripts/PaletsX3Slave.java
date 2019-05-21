15f5621ea9ff3f1dfdb3b4f14362cdf1a6c0ff3d
bapt117 <baptiste.ghirardi@telecom-sudparis.eu>
2019-05-21 22:38:21 +0200

    ajout d'offsets dans X3

--- T3HL-slave/src/main/java/scripts/PaletsX3Slave.java
+++ T3HL-slave/src/main/java/scripts/PaletsX3Slave.java
@@ -18,6 +18,8 @@ public class PaletsX3Slave extends Script{
 
     private int xEntry = 1500-230;// 1338
     private int yEntry = 1700 ;//+  (int) ConfigData.ROBOT_RAY.getDefaultValue() ;
+    private int offsetX = 0;
+    private int offsetY = 0;
     /**
      * constante
      */
@@ -32,7 +34,7 @@ public class PaletsX3Slave extends Script{
     public void execute(Integer version) {
         try {
             recalage();
-            robot.followPathTo(new VectCartesian(1338, yEntry));
+            robot.followPathTo(new VectCartesian(1338+offsetX, yEntry+offsetY));
 
             if(!symetrie) {
                 robot.turn(Math.PI);
@@ -40,32 +42,17 @@ public class PaletsX3Slave extends Script{
             else {
                 robot.turn(0);
             }
-            robot.useActuator(ActuatorsOrder.ACTIVE_LA_POMPE_DU_SECONDAIRE);
-            robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_DU_SECONDAIRE);
-            robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_DISTRIBUTEUR, true);//TODO refaire position bras
-            robot.useActuator(ActuatorsOrder.REMONTE_LE_BRAS_DU_SECONDAIRE_DU_DISTRIBUTEUR_VERS_ASCENSEUR, true);
-            robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DU_SECONDAIRE, true);
-            robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_DU_SECONDAIRE_DE_UN_PALET);
+
+            getPuck();
             robot.pushPaletDroit(CouleurPalet.BLEU); // TODO
-
             robot.moveLengthwise(-100,false);
 
-            robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_DISTRIBUTEUR, true);
-            robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_DU_SECONDAIRE, true);
-            robot.useActuator(ActuatorsOrder.REMONTE_LE_BRAS_DU_SECONDAIRE_DU_DISTRIBUTEUR_VERS_ASCENSEUR, true);
-            robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DU_SECONDAIRE, true);
-            robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_DU_SECONDAIRE_DE_UN_PALET);
+            getPuck();
             robot.pushPaletDroit(CouleurPalet.VERT); // TODO
-
             robot.moveLengthwise(-100,false);
 
-            robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_DISTRIBUTEUR, true);
-            robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_DU_SECONDAIRE, true);
-            robot.useActuator(ActuatorsOrder.REMONTE_LE_BRAS_DU_SECONDAIRE_DU_DISTRIBUTEUR_VERS_ASCENSEUR, true);
-            robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DU_SECONDAIRE, true);
-            robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_DU_SECONDAIRE_DE_UN_PALET, true);
+            getPuck();
             robot.pushPaletDroit(CouleurPalet.ROUGE); // TODO
-
             robot.useActuator(ActuatorsOrder.DESACTIVE_LA_POMPE_DU_SECONDAIRE);
 
         }
@@ -92,9 +79,19 @@ public class PaletsX3Slave extends Script{
         }
     }
 
+
+    private void getPuck() {
+        robot.useActuator(ActuatorsOrder.ACTIVE_LA_POMPE_DU_SECONDAIRE);
+        robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_DU_SECONDAIRE);
+        robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_DISTRIBUTEUR, true);//TODO refaire position bras
+        robot.useActuator(ActuatorsOrder.REMONTE_LE_BRAS_DU_SECONDAIRE_DU_DISTRIBUTEUR_VERS_ASCENSEUR, true);
+        robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DU_SECONDAIRE, true);
+        robot.useActuator(ActuatorsOrder.DESCEND_ASCENSEUR_DU_SECONDAIRE_DE_UN_PALET);
+    }
+
     @Override
     public Vec2 entryPosition(Integer version) {
-        return new VectCartesian(xEntry, yEntry);
+        return new VectCartesian(xEntry+offsetX, yEntry+offsetY);
     }
 
     @Override
