da494e11a006cfb3e46b75b4d7571c49e742b360
rene-INTech <remi.lamiaux@telecom-sudparis.eu>
2019-05-21 14:30:38 +0200

    virgule en fin de ligne ou en début de ligne suivante?

--- gagnerLaCoupe.py
+++ gagnerLaCoupe.py
@@ -35,7 +35,7 @@ os.system('wall "démarrage"')
 os.system("sudo killall -9 java")
 os.system("/home/pi/TechTheTachyon-HighLevel/run_master_from_python.sh")
 
-
+"""
 while True:
     while GPIO.input(8) == GPIO.LOW:
         wait()
@@ -45,3 +45,4 @@ while True:
         wait()
     os.system("/home/pi/TechTheTachyon-HighLevel/run_master_from_python.sh")
     os.system('wall "HL relancé"')
+"""
