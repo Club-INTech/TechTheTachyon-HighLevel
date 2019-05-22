d68cfe0aad05fe6c1eb913cd32f34c6c3602528b
jglrxavpok <jglrxavpok@gmail.com>
2019-05-22 22:27:48 +0200

    ça compile pas!

--- T3HL-master/src/main/java/scripts/VideDansZoneDepartSiProbleme.java
+++ T3HL-master/src/main/java/scripts/VideDansZoneDepartSiProbleme.java
@@ -82,24 +82,22 @@ public class VideDansZoneDepartSiProbleme extends Script {
 
 
             while (robot.getNbPaletsGauches() > 0) {
-                    robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_GAUCHE, true);
-                    robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_AU_DESSUS_ZONE_DEPART, true);
-                    robot.turn(-Math.PI/4);
-                    if (robot.getNbPaletsGauches() > 1) {
-                        SensorState.LEFT_ELEVATOR_MOVING.setData(true);
-                        robot.useActuator(ActuatorsOrder.MONTE_ASCENCEUR_GAUCHE_DE_UN_PALET,true);
-                    }
-                    robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_GAUCHE, true);
+                robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_GAUCHE, true);
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_AU_DESSUS_ZONE_DEPART, true);
+                robot.turn(-Math.PI / 4);
+                if (robot.getNbPaletsGauches() > 1) {
+                    SensorState.LEFT_ELEVATOR_MOVING.setData(true);
+                    robot.useActuator(ActuatorsOrder.MONTE_ASCENCEUR_GAUCHE_DE_UN_PALET, true);
+                }
+                robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_GAUCHE, true);
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_DE_LA_POSITION_AU_DESSUS_ZONE_DEPART_A_STOCKAGE, true);
-                    robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_ASCENSEUR, true);
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_ASCENSEUR, true);
                 robot.increaseScore(1);
-                if (robot.getLeftElevatorOrNull().peek() == CouleurPalet.ROUGE)
-                ){
+                if (robot.getLeftElevatorOrNull().peek() == CouleurPalet.ROUGE) {
                     robot.increaseScore(5);
-                };
                     robot.popPaletGauche();
                 }
-
+            }
             robot.useActuator(ActuatorsOrder.DESACTIVE_LA_POMPE_GAUCHE,true);
 
         } catch (UnableToMoveException e) {
