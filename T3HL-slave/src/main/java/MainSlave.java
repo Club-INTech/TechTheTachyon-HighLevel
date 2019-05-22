4dbe38c72424209693be65e96f82307dc2903af3
elomhs <eloise.maheas@telecom-sudparis.eu>
2019-05-22 05:23:26 +0200

    on met une cale sur le coté

--- T3HL-slave/src/main/java/MainSlave.java
+++ T3HL-slave/src/main/java/MainSlave.java
@@ -85,7 +85,7 @@ public class MainSlave extends RobotEntryPoint {
         robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_ASCENSEUR,true);
 
         robot.setRotationSpeed(Speed.SLOW_ALL);
-        Vec2 newPos = new VectCartesian(1500-242, 150);
+        Vec2 newPos = new VectCartesian(1500-242, 290);
         // position de démarrage, on s'oriente pour pouvoir prendre le palet rouge
         Vec2 pos = new VectCartesian(1500-300-10, 300+100+10);
         double targetAngle;
