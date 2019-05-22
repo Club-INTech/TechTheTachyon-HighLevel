6a8cf7954c0c5d6242e8b79ce18671b9234a66e8
naruaxe94 <naruaxe94@github.com>
2019-05-22 10:20:32 +0200

    variante VideDansZoneDepartSiProbleme

--- T3HL-master/src/main/java/scripts/VideDansZoneDepartSiProbleme.java
+++ T3HL-master/src/main/java/scripts/VideDansZoneDepartSiProbleme.java
@@ -9,6 +9,8 @@ import robot.Master;
 import utils.math.Vec2;
 import utils.math.VectCartesian;
 
+import java.math.MathContext;
+
 /**
  * Script pour vider les ascenseurs dans la zone de départ si l'accélérateur est bloqué
  */
@@ -21,7 +23,7 @@ public class VideDansZoneDepartSiProbleme extends Script {
     @Override
     public void execute(Integer version) {
         try {
-            robot.turn(0);
+            robot.turn(Math.PI/2);
         } catch (UnableToMoveException e) {
             e.printStackTrace();
         }
@@ -49,7 +51,7 @@ public class VideDansZoneDepartSiProbleme extends Script {
             robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_ASCENSEUR);
         }
 
-        if(robot.getNbPaletsGauches() > 0) {
+        /*if(robot.getNbPaletsGauches() > 0) {
             robot.waitWhileTrue(SensorState.LEFT_ELEVATOR_MOVING::getData);
             robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_GAUCHE);
             robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_AU_DESSUS_ZONE_DEPART, true);
@@ -60,12 +62,12 @@ public class VideDansZoneDepartSiProbleme extends Script {
             if(couleur == CouleurPalet.ROUGE){robot.increaseScore(6);}
             else {robot.increaseScore(1);}
             robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_ASCENSEUR);
-        }
+        }*/
     }
 
     @Override
     public Vec2 entryPosition(Integer version) {
-        return new VectCartesian(1300, 600);
+        return new VectCartesian(900, 500);
     }
 
     @Override
