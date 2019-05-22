e8f69de53e1cc3781e4de4ff1965b3cd23b3eb2f
naruaxe94 <naruaxe94@github.com>
2019-05-22 21:17:57 +0200

    VideDansZoneDepartSiProbleme version accélerateur test de rotataion

--- T3HL-master/src/main/java/scripts/VideDansZoneDepartSiProbleme.java
+++ T3HL-master/src/main/java/scripts/VideDansZoneDepartSiProbleme.java
@@ -54,17 +54,17 @@ public class VideDansZoneDepartSiProbleme extends Script {
             recalageRight.join();
             while (robot.getNbPaletsDroits() > 0) {
                 robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_DROITE, true);
-                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_AU_DESSUS_ZONE_DEPART, true);
+                //robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_AU_DESSUS_ZONE_DEPART, true);
 
-                robot.increaseScore(10); /* A CHANGER */
+                robot.increaseScore(1); /* A CHANGER */
 
                 if (robot.getNbPaletsDroits() > 1) {
                     SensorState.RIGHT_ELEVATOR_MOVING.setData(true);
                     robot.useActuator(ActuatorsOrder.MONTE_ASCENCEUR_DROIT_DE_UN_PALET,true);
                 }
                 robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DROITE, true);
-                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_DE_LA_POSITION_AU_DESSUS_ZONE_DEPART_A_STOCKAGE, true);
-                //robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_ASCENSEUR, true);
+                //robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_DE_LA_POSITION_AU_DESSUS_ZONE_DEPART_A_STOCKAGE, true);
+                robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_ASCENSEUR, true);
 
                 robot.popPaletDroit();
             }
@@ -77,17 +77,17 @@ public class VideDansZoneDepartSiProbleme extends Script {
 
             while (robot.getNbPaletsGauches() > 0) {
                     robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_GAUCHE, true);
-                    robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_AU_DESSUS_ZONE_DEPART, true);
+                    //robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_AU_DESSUS_ZONE_DEPART, true);
 
-                    robot.increaseScore(10); /* A CHANGER */
-
+                    robot.increaseScore(1); /* A CHANGER */
+                    robot.turn(-Math.PI/4);
                     if (robot.getNbPaletsDroits() > 1) {
                         SensorState.LEFT_ELEVATOR_MOVING.setData(true);
                         robot.useActuator(ActuatorsOrder.MONTE_ASCENCEUR_GAUCHE_DE_UN_PALET,true);
                     }
                     robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_GAUCHE, true);
-                    robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_DE_LA_POSITION_AU_DESSUS_ZONE_DEPART_A_STOCKAGE, true);
-                    //robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_ASCENSEUR, true);
+                    //robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_DE_LA_POSITION_AU_DESSUS_ZONE_DEPART_A_STOCKAGE, true);
+                    robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_ASCENSEUR, true);
 
                     robot.popPaletGauche();
                 }
