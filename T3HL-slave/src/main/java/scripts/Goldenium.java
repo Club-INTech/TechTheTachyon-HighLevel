a723b331fbbc11b5e54bc23db092fc9a0bf624b3
elomhs <eloise.maheas@telecom-sudparis.eu>
2019-05-24 05:15:23 +0200

    recalage goldenium

--- T3HL-slave/src/main/java/scripts/Goldenium.java
+++ T3HL-slave/src/main/java/scripts/Goldenium.java
@@ -108,8 +108,12 @@ public class Goldenium extends Script {
         */
 
 
-
-
+        try {
+            robot.turn(-Math.PI/2);
+        } catch (UnableToMoveException e) {
+            e.printStackTrace();
+        }
+        robot.recalageMeca(200);
         robot.useActuator(ActuatorsOrder.ACTIVE_LA_POMPE_DU_SECONDAIRE);
         robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DU_SECONDAIRE_A_LA_POSITION_GOLDONIUM,true);
         robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_DU_SECONDAIRE,true);
