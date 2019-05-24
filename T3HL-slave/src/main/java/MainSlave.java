f54f8bb9189393f22c17f38734c6a91652c8c5de
gwen <gwendoline.serandour@telecom-sudparis.eu>
2019-05-24 03:35:50 +0200

    same test 3

--- T3HL-slave/src/main/java/MainSlave.java
+++ T3HL-slave/src/main/java/MainSlave.java
@@ -133,7 +133,7 @@ public class MainSlave extends RobotEntryPoint {
         orderWrapper.waitJumper();
 
         robot.disableRotation();
-        robot.moveLengthwise(300, false);
+        robot.moveLengthwise(300, true);
 
         try {
             container.getService(MatchSlave.class).goToThenExecute(0);
