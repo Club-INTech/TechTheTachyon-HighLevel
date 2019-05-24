0a898629a477ff6443b7cc1a6c72ab1ff07d2b8b
elomhs <eloise.maheas@telecom-sudparis.eu>
2019-05-24 02:44:09 +0200

    ajustement paletx3

--- T3HL-slave/src/main/java/scripts/PaletsX3Slave.java
+++ T3HL-slave/src/main/java/scripts/PaletsX3Slave.java
@@ -26,7 +26,7 @@ public class PaletsX3Slave extends Script{
     private double offsetY;
     private static final int DISTANCE_INTER_PUCK = 100;
     private int xFirstPuck=1500-230; //1235;
-    private int yFirstPuck=1670;
+    private int yFirstPuck=1670-40;
 
     /**
      * constante
