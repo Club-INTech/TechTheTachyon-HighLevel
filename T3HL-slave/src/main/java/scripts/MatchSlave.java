f8f6200a81f153766840d115b8c357ba1f96b3ba
gwen <gwendoline.serandour@telecom-sudparis.eu>
2019-05-25 03:04:31 +0200

    nope

--- T3HL-slave/src/main/java/scripts/MatchSlave.java
+++ T3HL-slave/src/main/java/scripts/MatchSlave.java
@@ -28,16 +28,14 @@ public class MatchSlave extends Script {
         }
        robot.recalageMeca();
         */
-
-        scriptManagerSlave.getScript(ScriptNamesSlave.PALETSX3).goToThenExecute(0);
         // 1. Rush Bleu Accélérateur
         // 2. Pousse le palet bleu
-        /*scriptManagerSlave.getScript(ScriptNamesSlave.GETREDDEP).timedExecute(0);
+        scriptManagerSlave.getScript(ScriptNamesSlave.GETREDDEP).timedExecute(0);
         scriptManagerSlave.getScript(ScriptNamesSlave.GETBLUEACC).goToThenExecute(0);
         scriptManagerSlave.getScript(ScriptNamesSlave.GOLDENIUM).execute(0);
         scriptManagerSlave.getScript(ScriptNamesSlave.PALETSX3).goToThenExecute(0);
         table.addTassot();
-        scriptManagerSlave.getScript(ScriptNamesSlave.CRACHEUR).goToThenExecute(0);*/
+        scriptManagerSlave.getScript(ScriptNamesSlave.CRACHEUR).goToThenExecute(0);
 
         if(true)
             return;
