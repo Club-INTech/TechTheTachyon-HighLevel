6a6a8556aaa0c1847c21c51394220d45527fadad
jglrxavpok <jglrxavpok@gmail.com>
2019-05-26 12:18:21 +0200

    Correction ordre commandes pour reset les routes réseaux

--- run_master_from_python.sh
+++ run_master_from_python.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sudo killall java
 sudo fuser -k 19500
+./reset_internet
 cd bin/
-sudo route delete default gw 0.0.0.0 eth0
-sudo route delete default gw 0.0.0.0 wlan1
 sudo route add -net 192.168.0.0 netmask 255.255.255.0 eth0
 java -jar TTHL-master-1.0.jar
