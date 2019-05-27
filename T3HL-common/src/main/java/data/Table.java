2f3298d3157e05613f0e0c2e0e8cfd7ea9758ef7
jglrxavpok <jglrxavpok@gmail.com>
2019-05-28 00:54:06 +0200

    On utilise la position réelle du buddy pour le positionner sur la table

--- T3HL-common/src/main/java/data/Table.java
+++ T3HL-common/src/main/java/data/Table.java
@@ -124,6 +124,7 @@ public class Table implements Service {
      */
     private StillCircularRectangularObstacle balanceAndRamps;
 
+    private MobileCircularObstacle slaveObstacle;
 
     /**
      * Constructeur de la table
@@ -303,7 +304,10 @@ public class Table implements Service {
                 while (pointIterator.hasNext()) {
                     point = pointIterator.next();
                     if (obstacle.isInObstacle(point)) {
-                        obstacle.update(point);
+                        // si c'est pas le secondaire, on met à jour la position de l'obstacle
+                        if (point.distanceTo(XYO.getBuddyInstance().getPosition()) >= compareThreshold) {
+                            obstacle.update(point);
+                        }
                         Log.LIDAR.debug("MàJ de l'obstacle mobile : " + obstacle);
                         pointIterator.remove();
                     }
@@ -318,7 +322,8 @@ public class Table implements Service {
         for (Vec2 pt : points) {
             int ray = ennemyRobotRay;
             if (pt.distanceTo(XYO.getBuddyInstance().getPosition()) < compareThreshold) {
-                ray = buddyRobotRay;
+                Log.LIDAR.debug("Obstacle mobile non ajouté (car c'est le secondaire) de centre: " + pt);
+                continue;
             }
             MobileCircularObstacle obst = new MobileCircularObstacle(pt, ray+robotRay);
             Log.LIDAR.debug("Obstacle mobile ajouté : " + obst);
@@ -326,6 +331,7 @@ public class Table implements Service {
         }
 
         synchronized (mobileObstacles) {
+            mobileObstacles.add(slaveObstacle);
             mobileObstacles.addAll(mobileObstacleBuffer); // on envoie tout d'un coup
         }
 
@@ -654,6 +660,9 @@ public class Table implements Service {
         this.length = config.getInt(ConfigData.TABLE_X);
         this.width = config.getInt(ConfigData.TABLE_Y);
         this.compareThreshold = config.getInt(ConfigData.VECTOR_COMPARISON_THRESHOLD);
+
+        // on clone pas la position pour que l'obstacle se déplace comme par magie
+        this.slaveObstacle = new MobileCircularObstacle(XYO.getBuddyInstance().getPosition(), buddyRobotRay+robotRay);
     }
 
     public Obstacle getPaletRougeDroite() {
