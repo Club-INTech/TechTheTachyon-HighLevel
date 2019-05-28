9f8d1c70c49580c79b865d89db57a8c673b090cc
jglrxavpok <jglrxavpok@gmail.com>
2019-05-28 15:30:23 +0200

    Range ton bras et va au bon endroit stp

--- T3HL-master/src/main/java/scripts/X6alter.java
+++ T3HL-master/src/main/java/scripts/X6alter.java
@@ -161,8 +161,9 @@ public class X6alter extends Script {
                     robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_DROITE, true); // on a lâché le palet
 
                     try {
+                        robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_DROIT_A_LA_POSITION_DEPOT, true);
                         robot.turn(0);
-                        robot.moveLengthwise(-400,false);
+                        robot.moveLengthwise(400,false);
                         syncBuddy.sendBalanceFree();
                         try {
                             robot.turnToPoint(container.getService(Accelerateur.class).entryPosition(Match.ACC_VERSION));
