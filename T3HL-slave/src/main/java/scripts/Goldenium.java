f2926bb5671e68cde4ccc52f07eb4164de930464
gwen <gwendoline.serandour@telecom-sudparis.eu>
2019-05-31 12:08:10 +0200

    tourne vers plus quz pi/2 pour x3

--- T3HL-slave/src/main/java/scripts/Goldenium.java
+++ T3HL-slave/src/main/java/scripts/Goldenium.java
@@ -232,7 +232,7 @@ public class Goldenium extends Script {
             else{
                 robot.moveLengthwise(60, false);
             }
-            robot.turn(-Math.PI/2);
+            robot.turn(-Math.PI/2-0.1);
             robot.moveLengthwise(200,false);
 /*            robot.softGoTo(new VectCartesian(200,750),false);
             robot.softGoTo(new VectCartesian(1200,750),false);
