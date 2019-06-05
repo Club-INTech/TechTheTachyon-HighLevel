b811cf86561746eae4c66bea2a343088fa606304
rene-INTech <remi.lamiaux@telecom-sudparis.eu>
2019-06-05 12:12:57 +0200

    C'est mieux quand on oublie pas les fichiers

---
+++ T3HL-slave/src/main/java/scripts/OpenTheGateSlave.java
@@ -0,0 +1,82 @@
+package scripts;
+
+import data.Table;
+import data.XYO;
+import locomotion.UnableToMoveException;
+import pfg.config.Config;
+import robot.Slave;
+import utils.ConfigData;
+import utils.math.Vec2;
+import utils.math.VectCartesian;
+
+import static utils.Offsets.ZDD_X_JAUNE;
+import static utils.Offsets.ZDD_X_VIOLET;
+
+public class OpenTheGateSlave extends Script {
+    private boolean symetry;
+
+    /**
+     * Construit un script
+     *
+     * @param robot le robot
+     * @param table
+     */
+    protected OpenTheGateSlave(Slave robot, Table table) {
+        super(robot, table);
+    }
+
+    @Override
+    public void execute(Integer version) {
+        double offX;
+        if (symetry){
+            offX= ZDD_X_VIOLET.get();
+        } else {
+            offX= ZDD_X_JAUNE.get();
+        }
+        Vec2 zoneDep = new VectCartesian(1500-191-65+offX-20,1040-300);
+        Vec2 lowerLeft = new VectCartesian(650, 300);
+        Vec2 lowerRight = new VectCartesian(zoneDep.getX(), 300);
+        Vec2 upperLeft = new VectCartesian(650, 1300);
+        Vec2 upperRight = new VectCartesian(zoneDep.getX(), 1300);
+
+        while (true) {
+            try {
+                robot.followPathTo(upperRight, 0);
+            } catch (UnableToMoveException e) {
+                e.printStackTrace();
+            }
+            try {
+                robot.followPathTo(upperLeft, 0);
+            } catch (UnableToMoveException e) {
+                e.printStackTrace();
+            }
+            try {
+                robot.followPathTo(lowerLeft, 0);
+            } catch (UnableToMoveException e) {
+                e.printStackTrace();
+            }
+            try {
+                robot.followPathTo(lowerRight, 0);
+            } catch (UnableToMoveException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    @Override
+    public Vec2 entryPosition(Integer version) {
+        return XYO.getRobotInstance().getPosition();
+    }
+
+    @Override
+    public void updateConfig(Config config) {
+        super.updateConfig(config);
+        symetry = config.getString(ConfigData.COULEUR).equals("violet");
+    }
+
+    @Override
+    public void finalize(Exception e) {
+
+    }
+}
