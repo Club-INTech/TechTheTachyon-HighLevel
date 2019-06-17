f5d52f2e7071db904d6cbb573a5304985cb4c312
Yousra CHERKAOUI-TANGI <yousra.cherkaoui-tangi@telecom-sudparis.eu>
2019-06-17 17:16:51 +0200

    on sleep entre la position intermédiaire et finale

--- T3HL-master/src/main/java/scripts/ScriptPaletsZoneChaos.java
+++ T3HL-master/src/main/java/scripts/ScriptPaletsZoneChaos.java
@@ -76,6 +76,7 @@ public class ScriptPaletsZoneChaos extends Script {
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_SOL, true);
                 robot.useActuator(ActuatorsOrder.DESACTIVE_ELECTROVANNE_GAUCHE, true);
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_INTERMEDIAIRE, true);
+                Thread.sleep(300);
                 robot.useActuator(ActuatorsOrder.ENVOIE_LE_BRAS_GAUCHE_A_LA_POSITION_DEPOT, true);
                 robot.useActuator(ActuatorsOrder.ACTIVE_ELECTROVANNE_GAUCHE, false);
 
@@ -113,6 +114,8 @@ public class ScriptPaletsZoneChaos extends Script {
             }
             robot.useActuator(ActuatorsOrder.DESACTIVE_LA_POMPE_GAUCHE);
         } catch (UnableToMoveException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
